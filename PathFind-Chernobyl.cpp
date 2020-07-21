0ca50afa3d3529f6445718df1f7e3ef765bec83e
David Ryan <davidryn6@gmail.com>
2020-07-21 16:50:59 -0500

    Initial testing

--- PathFind-Chernobyl.cpp
+++ PathFind-Chernobyl.cpp
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <conio.h>
+#include <map>
 #include <unordered_map>
 #include <utility>
 
@@ -42,6 +43,28 @@ void mapPrint(unordered_map<pair<int, int>, int, hash_pair> m)
              << p.second << "\n";
 }
 
+void matrixFromMapPrint(unordered_map<pair<int, int>, int, hash_pair> map, int n, int m) {
+
+
+    const int cn = n;
+    const int cm = m;
+
+    int matrix[cn][cm];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            
+            pair<int, int> p(i, j);
+            matrix[i][j] = map[p];
+        }
+    }
+
+matrixPrint(&matrix[0][0], n, m);
+
+}
+
 int main()
 {
 
@@ -52,7 +75,7 @@ int main()
     // area of matrix
     const int area = n * m;
 
-    pair<int,int> playerLocation(4,3);
+    pair<int, int> playerLocation(4, 3);
 
     int matrix[n][m];
     unordered_map<pair<int, int>, int, hash_pair> pointMap;
@@ -73,22 +96,56 @@ int main()
 
     int step = 1;
 
-    
+    map<int, pair<int, int>> previousPoints;
+    map<int, pair<int, int>> currentPoints;
 
-    pair<int,int> up((playerLocation.first + 1),playerLocation.second);
-    pair<int,int> down((playerLocation.first - 1),playerLocation.second);
-    pair<int,int> left(playerLocation.first,(playerLocation.second - 1));
-    pair<int,int> right(playerLocation.first,(playerLocation.second + 1));
+    pair<int, int> up((playerLocation.first + 1), playerLocation.second);
+    pair<int, int> down((playerLocation.first - 1), playerLocation.second);
+    pair<int, int> left(playerLocation.first, (playerLocation.second - 1));
+    pair<int, int> right(playerLocation.first, (playerLocation.second + 1));
 
-    pointMap[up] = 1;
-    pointMap[down] = 1;
-    pointMap[left] = 1;
-    pointMap[right] = 1;
+    previousPoints[1] = up;
+    previousPoints[2] = down;
+    previousPoints[3] = left;
+    previousPoints[4] = right;
 
+    //while (!previousPoints.empty())
+    //{
+        int currentPointIndex = 0;
+        for (auto p : previousPoints)
+        {
+            pair<int, int> up((p.second.first + 1), p.second.second);
+            pair<int, int> down((p.second.first - 1), p.second.second);
+            pair<int, int> left(p.second.first, (p.second.second - 1));
+            pair<int, int> right(p.second.first, (p.second.second + 1));
+            if (pointMap[up] == 0) {
+                pointMap[up] = step;
+                currentPoints[currentPointIndex] = up;
+                currentPointIndex++;
+            }
+            if (pointMap[down] == 0) {
+                pointMap[down] = step;
+                currentPoints[currentPointIndex] = down;
+                currentPointIndex++;
+            }
+            if (pointMap[left] == 0) {
+                pointMap[left] = step;
+                currentPoints[currentPointIndex] = left;
+                currentPointIndex++;
+            }
+            if (pointMap[right] == 0) {
+                pointMap[right] = step;
+                currentPoints[currentPointIndex] = right;
+                currentPointIndex++;
+            }
+        }
 
+        previousPoints = currentPoints;
+        step++;
+
+    //}
 
     mapPrint(pointMap);
-
 
     return 0;
 }
